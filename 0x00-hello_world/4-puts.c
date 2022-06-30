825b2d1ece427ca163df1b1779a8fb2347024bf1
innthomas <innthomas00158@gmail.com>
2022-06-30 02:46:58 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * main function starts here
- */
-
+ *  main - Entry point
+ *  Return: Always 0 (Success)
+     */
 int main(void)
-	/* puts function goes here */
 {
-   puts("\"Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle");
    return (0);
 }
